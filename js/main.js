d8d509209f1d711e341af365e8645491b151ad3f
Romain Tello <romain.tello@gmail.com>
2020-02-25 10:46:46 +0100

    Add error message display for password

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 // Récupère le champ mot de passe
 var passwordField = document.getElementById('password');
+// Récupère la zone d'erreur du mot de passe
+var passwordError = document.getElementById('password-error');
+
 
 // Associe une action au fait de cliquer en-dehors du champ
-passwordField.onblur = function() {
+passwordField.onchange = function() {
   // Si le contenu actuel du champ a une longueur inférieure à 4
   if (passwordField.value.length < 4) {
-    // Afficher un message
-    console.log("Mot de passe trop court");
+    // Modifie le message d'erreur
+    passwordError.innerText = "Mot de passe trop court";
+    // Enlève la classe qui rend le message d'erreur invisible
+    passwordError.classList.remove('d-none');
   // Sinon
   } else {
-    // Afficher un autre message
-    console.log("Mot de passe valide");
+    // Modifie le message d'erreur
+    passwordError.innerText = "";
+    // Rajoute la classe qui rend le message d'erreur invisible
+    passwordError.classList.add('d-none');
+    
   }
 }
