acd56f1792b90a871df4b7bf0f21ef1e86bfcf7c
Romain Tello <romain.tello@gmail.com>
2020-02-25 14:51:34 +0100

    Add form validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Objet stockant pour chaque champ, si son contenu est valide ou non
+var valid = {
+  'email': false,
+  'username': false,
+  'password': false,
+  'password-confirm': false,
+};
+
 // Récupère le champ mot de passe
 var passwordField = document.getElementById('password');
 // Récupère la zone d'erreur du mot de passe
@@ -8,12 +16,16 @@ var passwordError = document.getElementById('password-error');
 passwordField.onchange = function() {
   // Si le contenu actuel du champ a une longueur inférieure à 4
   if (passwordField.value.length < 4) {
+    // Invalide le champ mot de passe
+    valid['password'] = false;
     // Modifie le message d'erreur
     passwordError.innerText = "Mot de passe trop court";
     // Enlève la classe qui rend le message d'erreur invisible
     passwordError.classList.remove('d-none');
   // Sinon
   } else {
+    // Valide le champ mot de passe
+    valid['password'] = true;
     // Modifie le message d'erreur
     passwordError.innerText = "";
     // Rajoute la classe qui rend le message d'erreur invisible
@@ -30,11 +42,15 @@ var usernameError = document.getElementById('username-error');
 // Associe une action au fait de cliquer en-dehors du champ
 usernameField.onchange = function() {
   if (usernameField.value.includes(' ')) {
+    // Invalide le champ nom d'utilisateur
+    valid['username'] = false;
     // Modifie le message d'erreur
     usernameError.innerText = "Le nom d'utilisateur ne doit pas contenir d'espace";
     // Enlève la classe qui rend le message d'erreur invisible
     usernameError.classList.remove('d-none');
   } else {
+    // Valide le champ nom d'utilisateur
+    valid['username'] = true;
     // Modifie le message d'erreur
     usernameError.innerText = "";
     // Rajoute la classe qui rend le message d'erreur invisible
@@ -52,12 +68,16 @@ var passwordComfirmError = document.getElementById('password-confirm-error');
 // Associe une action au fait de cliquer en-dehors du champ
 passwordComfirmField.onchange = function () {
   if (passwordComfirmField.value === passwordField.value) {
+    // Valide le champ confirmation du mot de passe
+    valid['password-confirm'] = true;
     // Modifie le message d'erreur
     passwordComfirmError.innerText = "";
     // Rajoute la classe qui rend le message d'erreur invisible
     passwordComfirmError.classList.add('d-none');
   // Sinon
   } else {
+    // Invalide le champ confirmation du mot de passe
+    valid['password-confirm'] = false;
     // Modifie le message d'erreur
     passwordComfirmError.innerText = "Les mots de passe ne correspondent pas";
     // Enlève la classe qui rend le message d'erreur invisible
@@ -75,15 +95,37 @@ var emailError = document.getElementById('email-error');
 // Associe une action au fait de cliquer en-dehors du champ
 emailField.onchange = function () {
   if (emailField.value.match(/\S+@\S+\.\S+/)) {
+    // Valide le champ e-mail
+    valid['email'] = true;
     // Modifie le message d'erreur
     emailError.innerText = "";
     // Rajoute la classe qui rend le message d'erreur invisible
     emailError.classList.add('d-none');
   // Sinon
   } else {
+    // Invalide la champ e-mail
+    valid['email'] = false;
     // Modifie le message d'erreur
     emailError.innerText = "Il ne s'agit pas d'une adresse e-mail valide";
     // Enlève la classe qui rend le message d'erreur invisible
     emailError.classList.remove('d-none');
+  }
+}
+
+
+// Récupère le formulaire d'enregistrement
+var registrationForm = document.getElementById('registration-form');
+
+// Associe une action au fait d'envoyer le formulaire
+registrationForm.onsubmit = function(event) {
+  // Si tous les champs ont été validés
+  if (valid['username'] && valid['email'] && valid['password'] && valid['password-confirm']) {
+    // Envoie le formulaire normalement
+  // Sinon, si l'un des champs contient une erreur
+  } else {
+    // Empêche le formulaire de s'envoyer
+    event.preventDefault();
+    // Affiche une alerte à l'attention de l'utilisateur
+    window.alert("Merci de vérifier votre saisie!");
   }
 }
